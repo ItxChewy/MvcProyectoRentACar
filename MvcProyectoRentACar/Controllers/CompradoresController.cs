d795a62d2ed0567f746e580f2cbe5563b3d1b58e
ItxChewy <aarongarcia1906@gmail.com>
2025-03-02 13:19:56 +0100

    filtros funcionales

--- MvcProyectoRentACar/Controllers/CompradoresController.cs
+++ MvcProyectoRentACar/Controllers/CompradoresController.cs
@@ -34,6 +34,51 @@ namespace MvcProyectoRentACar.Controllers
 
         }
 
+
+        public async Task<IActionResult> FilterCoches(string search, string sort, string marcha, int? puertas, string combustible)
+        {
+            List<VistaCoche> coches = await this.repo.FindAllCochesAsync();
+
+            if (!string.IsNullOrEmpty(search))
+            {
+                coches = coches.Where(c => c.Marca.Contains(search, StringComparison.OrdinalIgnoreCase) || c.Modelo.Contains(search, StringComparison.OrdinalIgnoreCase)).ToList();
+            }
+
+            if (!string.IsNullOrEmpty(marcha))
+            {
+                coches = coches.Where(c => c.Marcha.Equals(marcha, StringComparison.OrdinalIgnoreCase)).ToList();
+            }
+
+            if (puertas.HasValue)
+            {
+                coches = coches.Where(c => c.Puertas == puertas.Value).ToList();
+            }
+
+            if (!string.IsNullOrEmpty(combustible))
+            {
+                coches = coches.Where(c => c.Combustible.Equals(combustible, StringComparison.OrdinalIgnoreCase)).ToList();
+            }
+
+            
+
+            switch (sort)
+            {
+                case "precio_asc":
+                    coches = coches.OrderBy(c => c.PrecioKilometros).ToList();
+                    break;
+                case "precio_desc":
+                    coches = coches.OrderByDescending(c => c.PrecioKilometros).ToList();
+                    break;
+            }
+
+            return View("Coches", coches);
+        }
+
+
+
+
+
+
         public async Task<IActionResult> DetailsCoche(int idcoche)
         {
             VistaCoche coche = await this.repo.GetVistaCocheAsync(idcoche);
